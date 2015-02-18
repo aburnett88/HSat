93a3b199399601ceec1e2535506b5c13dc5ce7ca
Andrew Burnett <andyburnett88@googlemail.com>
2015-02-18 17:33:00 +0000

    Added preamble for benchs-sr/Parsing/Main.hs

--- benchs-src/Parsing/Main.hs
+++ benchs-src/Parsing/Main.hs
@@ -1,3 +1,14 @@
+{-|
+Module      : Parsing.Main
+Description : The entry to the Parsing tests
+Copyright   : (c) Andrew Burnett 2014-2015
+Maintainer  : andyburnett88@gmail.com
+Stability   : experimental
+Portability : Unknown
+
+Provides Parsing benchmarks
+-}
+
 import Criterion.Main
 
 main :: IO ()
