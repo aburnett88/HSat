dd9c177ac3bf83e902ec610df8adb001b397bff1
Andrew Burnett <andyburnett88@googlemail.com>
2015-03-24 22:34:33 +0000

    Fixed spelling, added simple documentation. Introduced language extnesions to increase readability

--- src/HSat/Make.hs
+++ src/HSat/Make.hs
@@ -1,60 +1,77 @@
+{-# LANGUAGE LambdaCase #-}
+
 {-|
 Module      : HSat.Make
-Description : The Make module 
+Description : Make allows Problem's to be made
 Copyright   : (c) Andrew Burnett 2014-2015
 Maintainer  : andyburnett88@gmail.com
 Stability   : experimental
 Portability : Unknown
 
-This module provides access to random generation of 'Problem's
+This module allows access to a rich set of functions that allow 'Problem's
+to be created
 -}
 
 module HSat.Make (
-  make,    -- :: (MonadRandom m) => Config -> m Problem
+  -- * Make
+  make,     -- :: (MonadRandom m) => Config -> m Problem
   makeList, -- :: (MonadRandom m) = Int -> Config -> m [Problem]
+  -- * Error
   MakeError(..)
   ) where
 
+import Control.Applicative
+import Control.Monad (replicateM)
 import Control.Monad.Random.Class
+import Data.Bifunctor
+import HSat.Make.BSP.CNF
+import HSat.Make.BSP.CNF.Internal (CNFMakeError(..))
 import HSat.Make.Config
 import HSat.Problem
+import HSat.Problem.BSP.CNF
 import HSat.Problem.ProblemExpr
 import HSat.Problem.Source
-import Control.Monad (replicateM,liftM)
-import HSat.Make.BSP.CNF
-import Data.Bifunctor
-import HSat.Make.BSP.CNF.Internal (CNFMakeError(..))
-import HSat.Problem.BSP.CNF
 
+{-|
+This data type describes errors that can be thrown when creating 'Problem's.
+-}
 data MakeError =
+  -- ^ Describes problems' thrown when creating 'CNF' expressions
   CNFError CNFMakeError
   deriving (Eq,Show)
 
-liftCNF :: (Either CNFMakeError CNF) -> (Either MakeError ProblemExpr)
+liftCNF :: Either CNFMakeError CNF -> Either MakeError ProblemExpr
 liftCNF = bimap CNFError mkCNFProblem
 
 {-|
-Creates a random 'Problem' from a 'Config'
+Creates a random 'Problem' from a 'Config'.
+The Bool argument denotes whether errros are ignored in the Config - e.g.,
+some config's may not generate correct problems'. We can mitigate this, and
+generate them anyway but without them being poor
 -}
-make :: (MonadRandom m) => Config -> Bool -> m (Either MakeError Problem)
-make config ignoreErrors = do
-  problemExpr <-
-    case getInputConfig config of
-      CNFProblemType cnfConfig -> liftCNF `liftM` if ignoreErrors then
-                                    (Right . snd) `liftM` makeCNF' cnfConfig else
-                                    makeCNF cnfConfig
-  return $ second (makeProblemFromExpr config) problemExpr
+make                     :: (MonadRandom m,Applicative m) => Config -> Bool ->
+                            m (Either MakeError Problem)
+make config ignoreErrors =
+  second (makeProblemFromExpr config) <$>
+  case getInputConfig config of
+    CNFProblemType cnfConfig ->
+      liftCNF <$> if ignoreErrors then
+                    Right . snd <$> makeCNF' cnfConfig else
+                    makeCNF cnfConfig
 
-makeProblemFromExpr :: Config -> ProblemExpr -> Problem
+makeProblemFromExpr        :: Config -> ProblemExpr -> Problem
 makeProblemFromExpr config =
   mkProblem (mkMakeConfig config) . changeProblemType (getOutputType config)
 
-{-| Creates a list of Problem's from a given 'Config' -}
-
-makeList :: (MonadRandom m) => Int -> Config -> m [Problem]
+{-|
+Creates a list of 'Problem's from a given 'Config' and the number required.
+We ignore any errors and, if the config is invalid, then the closest valid
+config is generated
+-}
+makeList               :: (MonadRandom m,Applicative m) =>
+                          Int -> Config -> m [Problem]
 makeList number config =
-  map (\arg ->
-        case arg of
+  map (\case
           Left _ -> error "makeList error"
           Right problem -> problem)
-  `liftM` replicateM number (make config True)
+  <$> replicateM number (make config True)
