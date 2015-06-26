daa9d04a897f91f952dd2a750b56aca374e04f3b
aburnett88 <andyburnett88@googlemail.com>
2015-06-26 12:04:04 +0100

    Tidied up spelling errors in Instance/CNF

--- src/HSat/Solution/Instances/CNF.hs
+++ src/HSat/Solution/Instances/CNF.hs
@@ -15,24 +15,36 @@ Exports functionality for a solution type for CNF
 -}
 
 module HSat.Solution.Instances.CNF (
-  checkSolution,
-  emptySolution,
-  BoolSolution(..)
+  checkCNFSolution, -- :: CNF -> BoolSolution -> Bool
+  emptySolution   , -- :: BoolSolution
+  BoolSolution(..), 
   ) where
 
+import Data.Map                      (Map)
+import qualified Data.Map            as M
 import HSat.Problem.Instances.CNF
 import HSat.Problem.Instances.Common
-import Data.Map (Map)
-import qualified Data.Map as M
 import HSat.Solution.Class
 
 instance Solution CNF where
   type SolInstance CNF = BoolSolution
-  checkSolution = undefined
+  checkSolution        = checkCNFSolution
 
+{-|
+Checks a CNF against a solution and returns whether it is satisfied or not
+-}
+checkCNFSolution     :: CNF -> BoolSolution -> Bool
+checkCNFSolution _ _ = error "Solution.Instance.CNF:checkNFSolution not written"
+
+{-|
+A 'BoolSolution' consists of an internal 'Map' from 'Variable's to 'Sign's
+-}
 data BoolSolution = BoolSolution {
   solution :: Map Variable Sign
   } deriving (Eq,Show)
 
+{-|
+Returns an empty Solution
+-}
 emptySolution :: BoolSolution
 emptySolution = BoolSolution M.empty
